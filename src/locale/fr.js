c62769b1ba87848f7fb96acf16eda5f786cdf93e
iGoDLiK3 <41002701+iGoDLiK3@users.noreply.github.com>
2018-07-09 14:25:42 +0200

    feat(locale): French locale

---
+++ src/locale/fr.js
@@ -0,0 +1,14 @@
+export default {
+    dataIterator: {
+        rowsPerPageText: 'Données par page:',
+        rowsPerPageAll: 'Tous',
+        pageText: '{0}-{1} sur {2}',
+        noResultsText: 'Aucun résultat correspondant trouver',
+        nextPage: 'Page suivante',
+        prevPage: 'Page précédente'
+    },
+    dataTable: {
+        rowsPerPageText: 'Lignes par page:'
+    },
+    noDataText: 'Aucune donnée disponible'
+}
